7048f58780fa1167871804d6ece879cb644ab6b5
rishi-verma04 <rishi.yudi04@gmail.com>
2020-01-22 13:30:08 +0530

    added service in yaml file

---
+++ output/cucumber-pretty/report.js
@@ -0,0 +1,397 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("features/search.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Duck Duck Go Search",
+  "description": "",
+  "id": "duck-duck-go-search",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "I want to search on duck-duck-go site",
+  "description": "",
+  "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on the website duck-duck-go",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter the \"\u003ckeyword\u003e\" to search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "I navigate to videos search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should get minimum 2 search results",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 9,
+  "name": "",
+  "description": "",
+  "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;",
+  "rows": [
+    {
+      "cells": [
+        "keyword"
+      ],
+      "line": 10,
+      "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;1"
+    },
+    {
+      "cells": [
+        "java"
+      ],
+      "line": 11,
+      "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;2"
+    },
+    {
+      "cells": [
+        "webdriver"
+      ],
+      "line": 12,
+      "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;3"
+    },
+    {
+      "cells": [
+        "docker"
+      ],
+      "line": 13,
+      "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;4"
+    },
+    {
+      "cells": [
+        "kubernetes"
+      ],
+      "line": 14,
+      "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;5"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 2701631692,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "I want to search on duck-duck-go site",
+  "description": "",
+  "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on the website duck-duck-go",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter the \"java\" to search",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "I navigate to videos search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should get minimum 2 search results",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchSteps.launchSite()"
+});
+formatter.result({
+  "duration": 2681614241,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "java",
+      "offset": 13
+    }
+  ],
+  "location": "SearchSteps.enterKeyword(String)"
+});
+formatter.result({
+  "duration": 2095736704,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SearchSteps.navigateTovideos()"
+});
+formatter.result({
+  "duration": 234537786,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 21
+    }
+  ],
+  "location": "SearchSteps.verifySearchResults(int)"
+});
+formatter.result({
+  "duration": 701822362,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 76348770,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1003582654,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 12,
+  "name": "I want to search on duck-duck-go site",
+  "description": "",
+  "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on the website duck-duck-go",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter the \"webdriver\" to search",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "I navigate to videos search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should get minimum 2 search results",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchSteps.launchSite()"
+});
+formatter.result({
+  "duration": 2152849979,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "webdriver",
+      "offset": 13
+    }
+  ],
+  "location": "SearchSteps.enterKeyword(String)"
+});
+formatter.result({
+  "duration": 2044034386,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SearchSteps.navigateTovideos()"
+});
+formatter.result({
+  "duration": 243165134,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 21
+    }
+  ],
+  "location": "SearchSteps.verifySearchResults(int)"
+});
+formatter.result({
+  "duration": 667905568,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 66753098,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 993775877,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "I want to search on duck-duck-go site",
+  "description": "",
+  "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;4",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on the website duck-duck-go",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter the \"docker\" to search",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "I navigate to videos search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should get minimum 2 search results",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchSteps.launchSite()"
+});
+formatter.result({
+  "duration": 3733755988,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "docker",
+      "offset": 13
+    }
+  ],
+  "location": "SearchSteps.enterKeyword(String)"
+});
+formatter.result({
+  "duration": 2621472117,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SearchSteps.navigateTovideos()"
+});
+formatter.result({
+  "duration": 231922488,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 21
+    }
+  ],
+  "location": "SearchSteps.verifySearchResults(int)"
+});
+formatter.result({
+  "duration": 748762328,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 63796578,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 949984798,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "I want to search on duck-duck-go site",
+  "description": "",
+  "id": "duck-duck-go-search;i-want-to-search-on-duck-duck-go-site;;5",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on the website duck-duck-go",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter the \"kubernetes\" to search",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "I navigate to videos search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should get minimum 2 search results",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchSteps.launchSite()"
+});
+formatter.result({
+  "duration": 2688989847,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "kubernetes",
+      "offset": 13
+    }
+  ],
+  "location": "SearchSteps.enterKeyword(String)"
+});
+formatter.result({
+  "duration": 2369043077,
+  "status": "passed"
+});
+formatter.match({
+  "location": "SearchSteps.navigateTovideos()"
+});
+formatter.result({
+  "duration": 223342354,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 21
+    }
+  ],
+  "location": "SearchSteps.verifySearchResults(int)"
+});
+formatter.result({
+  "duration": 750664850,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 74501055,
+  "status": "passed"
+});
+});
